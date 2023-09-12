5cfebeb0c22cc29135c9f30abcae7c1dbfae2f94
paresiqbal <thecrowdel@gmail.com>
2023-09-12 22:42:37 +0700

    adasdas

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,8 +1,19 @@
 import Link from "next/link";
+import Image from "next/image";
+
+// assests
+import Logo from "./buah hati.png";
 
 export default function Navbar() {
   return (
     <nav>
+      <Image
+        src={Logo}
+        alt="klinikbuahhati"
+        width={40}
+        quality={100}
+        placeholder="blur"
+      ></Image>
       <h1>Klink Buah Hati</h1>
       <Link href="/">Dashboard</Link>
       <Link href="/tickets">Tickets</Link>
