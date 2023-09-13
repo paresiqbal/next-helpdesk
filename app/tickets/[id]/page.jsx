b90a2107da6e9fff707eb503aaa46f949ef7eb31
paresiqbal <thecrowdel@gmail.com>
2023-09-13 14:03:38 +0700

    Add notfound

--- app/tickets/[id]/page.jsx
+++ app/tickets/[id]/page.jsx
@@ -1,9 +1,29 @@
+import { notFound } from "next/navigation";
+
+export const dynamicParams = true;
+
+export async function generateStaticParams() {
+  const res = await fetch("http://localhost:4000/tickets");
+
+  const tickets = await res.json();
+
+  return tickets.map((ticket) => ({
+    id: ticket.id,
+  }));
+}
+
+// get tickets
 async function getTicket(id) {
   const res = await fetch("http://localhost:4000/tickets/" + id, {
     next: {
       revalidate: 60,
     },
   });
+
+  // check response
+  if (!res.ok) {
+    notFound();
+  }
 
   return res.json();
 }
